1705c2109a2a31a8825dbd4be4ba9760702271b1
ivanhack34 <ivanpolanco34@gmail.com>
2022-11-29 00:00:09 -0400

    crypto

---
+++ src/utils/crypto.js
@@ -0,0 +1,14 @@
+const bcrypt = require('bcrypt')
+
+const hashPassword = (plainPassword) => {
+    //? PlainPassword = contraseña en texto plano
+    return bcrypt.hashSync(plainPassword, 10)
+}
+
+console.log(hashPassword('root'));
+
+const comparePassword = (plainPassword, hashPassword) => {
+    return bcrypt.compareSync(plainPassword, hashPassword)
+}
+
+console.log(comparePassword('root', '$2b$10$wW1KGf/Gad5zNNZ/2niXZOijNiJGnlKSKwEBlXT4WzunltHGDBMN.'))
